9fee59822e6b9ac89fe84dc21a5dbfa728c662d4
D1074181045 <D1074181045@gm.lhu.edu.tw>
2021-06-28 13:53:46 +0800

    權限調整

--- app/src/main/java/com/example/bluetooth_mora/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_mora/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.bluetooth_mora;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final int REQUEST_PERMISSION_FOR_BLUETOOTH = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +41,61 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(it);
             }
         });
+
+        askForWriteExternalStoragePermission();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // 檢查收到的權限要求編號是否和我們送出的相同
+        if (requestCode == REQUEST_PERMISSION_FOR_BLUETOOTH) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "取得 BLUETOOTH 權限",
+                        Toast.LENGTH_SHORT)
+                        .show();
+                return;
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void askForWriteExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                android.Manifest.permission.BLUETOOTH) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // 這項功能尚未取得使用者的同意
+            // 開始執行徵詢使用者的流程
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    MainActivity.this,
+                    android.Manifest.permission.BLUETOOTH)) {
+                AlertDialog.Builder altDlgBuilder = new AlertDialog.Builder(MainActivity.this);
+                altDlgBuilder.setTitle("提示");
+                altDlgBuilder.setMessage("App需要啟用BLUETOOTH權限來連接");
+                altDlgBuilder.setIcon(android.R.drawable.ic_dialog_info);
+                altDlgBuilder.setCancelable(false);
+                altDlgBuilder.setPositiveButton("確定",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // 顯示詢問使用者是否同意功能權限的對話盒
+                                // 使用者答覆後會執行onRequestPermissionsResult()
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{
+                                                android.Manifest.permission.BLUETOOTH},
+                                        REQUEST_PERMISSION_FOR_BLUETOOTH);
+                            }
+                        });
+                altDlgBuilder.show();
+            } else {
+                // 顯示詢問使用者是否同意功能權限的對話盒
+                // 使用者答覆後會執行onRequestPermissionsResult()
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{
+                                android.Manifest.permission.BLUETOOTH},
+                        REQUEST_PERMISSION_FOR_BLUETOOTH);
+
+            }
+        }
     }
 }
